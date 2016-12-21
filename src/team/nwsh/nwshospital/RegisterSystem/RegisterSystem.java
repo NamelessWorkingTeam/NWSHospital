7d5244826de98e03c20a4312f93349460da096f5
LiuHao <511200255@qq.com>
2016-12-21 21:58:52 +0800

    liuhao 12-21 update 挂号处取消挂号收费分割 采用一个窗口来进行检索输入的身份证号是什么状态

--- src/team/nwsh/nwshospital/RegisterSystem/RegisterSystem.java
+++ src/team/nwsh/nwshospital/RegisterSystem/RegisterSystem.java
@@ -10,12 +10,13 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class RegisterSystem extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
-	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -45,55 +46,36 @@ public class RegisterSystem extends JFrame {
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 10, 245, 391);
+		panel.setBounds(50, 10, 435, 391);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JLabel label = new JLabel("\u6302\u53F7");
+		JLabel label = new JLabel("\u6302\u53F7\u5904");
 		label.setFont(new Font("Î¢ÈíÑÅºÚ", Font.BOLD, 16));
-		label.setBounds(95, 10, 50, 35);
+		label.setBounds(199, 10, 50, 35);
 		panel.add(label);
 		
 		JLabel label_2 = new JLabel("\u8BF7\u8F93\u5165\u8EAB\u4EFD\u8BC1\u53F7\uFF1A");
 		label_2.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 14));
-		label_2.setBounds(31, 90, 126, 35);
+		label_2.setBounds(162, 90, 126, 35);
 		panel.add(label_2);
 		
 		textField = new JTextField();
 		textField.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 14));
-		textField.setBounds(31, 135, 167, 21);
+		textField.setBounds(162, 135, 167, 21);
 		panel.add(textField);
 		textField.setColumns(10);
 		
 		JButton button = new JButton("\u786E\u5B9A");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Register register = new Register ();
+				register.setVisible(true);
+				register.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+			}
+		});
 		button.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 14));
-		button.setBounds(31, 166, 80, 27);
+		button.setBounds(162, 178, 80, 27);
 		panel.add(button);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(265, 10, 245, 391);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		JLabel label_1 = new JLabel("\u6536\u8D39");
-		label_1.setFont(new Font("Î¢ÈíÑÅºÚ", Font.BOLD, 16));
-		label_1.setBounds(106, 10, 50, 35);
-		panel_1.add(label_1);
-		
-		JLabel label_3 = new JLabel("\u8BF7\u8F93\u5165\u8EAB\u4EFD\u8BC1\u53F7\uFF1A");
-		label_3.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 14));
-		label_3.setBounds(30, 90, 126, 35);
-		panel_1.add(label_3);
-		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 14));
-		textField_1.setColumns(10);
-		textField_1.setBounds(31, 135, 167, 21);
-		panel_1.add(textField_1);
-		
-		JButton button_1 = new JButton("\u786E\u5B9A");
-		button_1.setFont(new Font("Î¢ÈíÑÅºÚ", Font.PLAIN, 14));
-		button_1.setBounds(30, 166, 80, 27);
-		panel_1.add(button_1);
 	}
 }
