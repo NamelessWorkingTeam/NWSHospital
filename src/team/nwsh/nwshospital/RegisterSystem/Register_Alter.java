7938cd5e7f2f37626f3bc539805a509f7cb08b4a
LiuHao <511200255@qq.com>
2016-12-23 23:51:43 +0800

    update by liuhao in 12-23 23:50 需要收费的预约病人 ok 已收费的预约病人 ok 已收费的挂号病人 ok 正在就诊 ok 就诊结束收费  ！！未解决！！！！ 挂号 ok

--- src/team/nwsh/nwshospital/RegisterSystem/Register_Alter.java
+++ src/team/nwsh/nwshospital/RegisterSystem/Register_Alter.java
@@ -10,18 +10,27 @@ import javax.swing.border.EmptyBorder;
 import team.nwsh.nwshospital.MySQLConnect;
 
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
 import javax.swing.DefaultComboBoxModel;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 
 public class Register_Alter extends JFrame {
 	
+	static String sec;
+	
 	static String P_ID;//上一页面获取的身份证号
+	static String P_NAME;
+	static String P_SEX;
+	static String P_AGE;//记得转换类型
+	static String P_TEL;
 	
 	private JPanel contentPane;
 
@@ -46,8 +55,69 @@ public class Register_Alter extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	//构造方法 传值 传 姓名、性别、年龄、电话。科室信息重新用if判断
 	public Register_Alter() {
 		P_ID = RegisterSystem.PAT_ID;//从上个页面获取身份证号传值给P_ID
+		
+		
+		//查询姓名
+		String sql_name= "SELECT PAT_NAME FROM PATIENTS WHERE PAT_ID='"+P_ID+"'";
+		MySQLConnect con_name= new MySQLConnect(sql_name);
+		try {
+			ResultSet ResultSet_NAME = con_name.pst.executeQuery();
+			while(ResultSet_NAME.next()){
+				P_NAME=ResultSet_NAME.getString("PAT_NAME");//将名字查询结果赋值给了P_NAME
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//查询性别
+		String sql_sex= "SELECT PAT_SEX FROM PATIENTS WHERE PAT_ID='"+P_ID+"'";
+		MySQLConnect con_sex= new MySQLConnect(sql_sex);
+		try {
+			ResultSet ResultSet_SEX = con_sex.pst.executeQuery();
+			while(ResultSet_SEX.next()){
+				P_SEX=ResultSet_SEX.getString("PAT_SEX");//将名字查询结果赋值给了P_SEX
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		//查询年龄
+				String sql_age= "SELECT PAT_AGE FROM PATIENTS WHERE PAT_ID='"+P_ID+"'";
+				MySQLConnect con_age= new MySQLConnect(sql_age);
+				try {
+					ResultSet ResultSet_AGE = con_age.pst.executeQuery();
+					while(ResultSet_AGE.next()){
+						P_AGE=ResultSet_AGE.getString("PAT_AGE");//将名字查询结果赋值给了P_AGE
+					}
+					
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		
+				
+				//查询电话
+				String sql_tel= "SELECT PAT_TEL FROM PATIENTS WHERE PAT_ID='"+P_ID+"'";
+				MySQLConnect con_tel= new MySQLConnect(sql_tel);
+				try {
+					ResultSet ResultSet_TEL = con_tel.pst.executeQuery();
+					while(ResultSet_TEL.next()){
+						P_TEL=ResultSet_TEL.getString("PAT_TEL");//将名字查询结果赋值给了P_AGE
+					}
+					
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		
+				
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 550, 500);
 		contentPane = new JPanel();
@@ -99,21 +169,25 @@ public class Register_Alter extends JFrame {
 		textField_NAME.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		textField_NAME.setBounds(202, 63, 136, 30);
 		panel_1.add(textField_NAME);
+		textField_NAME.setText(P_NAME);//表格填充名字
 		
 		JTextField textField_SEX = new JTextField();
 		textField_SEX.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		textField_SEX.setBounds(202, 103, 136, 30);
 		panel_1.add(textField_SEX);
+		textField_SEX.setText(P_SEX);//表格填充性别
 		
 		JTextField textField_AGE = new JTextField();
 		textField_AGE.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		textField_AGE.setBounds(202, 143, 136, 30);
 		panel_1.add(textField_AGE);
+		textField_AGE.setText(P_AGE);//表格填充年龄
 		
-		JTextField textField_PHONE = new JTextField();
-		textField_PHONE.setFont(new Font("微软雅黑", Font.PLAIN, 12));
-		textField_PHONE.setBounds(202, 183, 136, 30);
-		panel_1.add(textField_PHONE);
+		JTextField textField_TEL = new JTextField();
+		textField_TEL.setFont(new Font("微软雅黑", Font.PLAIN, 12));
+		textField_TEL.setBounds(202, 183, 136, 30);
+		panel_1.add(textField_TEL);
+		textField_TEL.setText(P_TEL);//表格填充电话
 		
 		JComboBox comboBox_KESHI = new JComboBox();
 		comboBox_KESHI.setModel(new DefaultComboBoxModel(new String[] {"\u666E\u901A\u5916\u79D1", "\u666E\u901A\u5185\u79D1", "\u4E94\u5B98\u79D1", "\u68C0\u9A8C\u79D1"}));
@@ -137,22 +211,33 @@ public class Register_Alter extends JFrame {
 		button_2.setFont(new Font("微软雅黑", Font.PLAIN, 12));
 		button_2.setBounds(312, 380, 93, 23);
 		contentPane.add(button_2);
-		
+		//按钮“确认修改并收费”
 		JButton button_1 = new JButton("\u786E\u8BA4\u9884\u7EA6\u5E76\u6536\u8D39");
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				if(comboBox_KESHI.getSelectedItem().toString().compareTo("普通外科") == 0) sec="5000";
+				if(comboBox_KESHI.getSelectedItem().toString().compareTo("普通内科") == 0) sec="5001";
+				if(comboBox_KESHI.getSelectedItem().toString().compareTo("五官科") == 0)	sec="5002";
+				if(comboBox_KESHI.getSelectedItem().toString().compareTo("检验科") == 0)	sec="5003";
 				
-				System.out.println(P_ID);
+//修改科室信息，并把状态STA_TUS置为1				
+				String sql = " UPDATE STATE SET SEC_ID='"+sec+"',STA_TUS=1 WHERE PAT_ID='"+P_ID+"' ";
 
-				String sql= "";
-				MySQLConnect con1= new MySQLConnect(sql);
+				MySQLConnect con = new MySQLConnect(sql);
 				try {
-					con1.pst.executeUpdate();
-				} catch (SQLException e) {
+					con.pst.executeUpdate();
+					JOptionPane.showMessageDialog(null, "数据库写入成功！", "提示",JOptionPane.INFORMATION_MESSAGE);
+				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
-					e.printStackTrace();
+					JOptionPane.showMessageDialog(null, "数据库写入失败！", "提示", JOptionPane.ERROR_MESSAGE);
+					e1.printStackTrace();
 				}
 				
+				
+				
+				
+				
+
 			}
 		});
 		button_1.setFont(new Font("微软雅黑", Font.PLAIN, 12));
